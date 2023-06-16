1233dcd08c62e3ec65c0f82dd1cde4d9e09d2204
atuljha381 <mail.atuljha@gmail.com>
2023-06-17 00:29:46 +0530

    New Fields Added to Customer Model

--- TSC-Backend/models/customer.model.js
+++ TSC-Backend/models/customer.model.js
@@ -8,11 +8,28 @@ const mongoose = require("mongoose");
  * Defining the Customer Schema
  */
 const customerSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phone: { type: Number, require: true },
-  email: { type: String, required: true, unique: true },
-  address: { type: String, required: true },
+  firstName: { type: String, required: [true, "First Name is required"] },
+  lastName: { type: String, required: [true, "Last Name is required"] },
+  phone: {
+    type: Number,
+    require: [true, "Phone Number is required"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  // password:{
+  //   type: String,
+  //   required: [true, "Password is required"],
+  //   minLength: [5],
+  // },
+  address: { type: String, required: [true, "Address is required"] },
+  city: { type: String, required: [true, "City is required"] },
+  state: { type: String, required: [true, "State is required"] },
+  country: { type: String, required: [true, "Country is required"] },
+  pincode: { type: String, required: [true, "pincode is required"] },
 });
 
 module.exports = Customer = mongoose.model("Customer", customerSchema);
