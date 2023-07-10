f4f45749c9669d1fb4b9a2994ad7746f87ea2779
atuljha381 <mail.atuljha@gmail.com>
2023-07-10 22:44:18 +0530

    Product Model, Controller, Routes implemented

---
+++ TSC-Backend/models/product.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: [true, "Enter Product Name"],
+    unique: true,
+  },
+  productPrice: { type: Number, required: [true, "Enter Product Price"] },
+  productDescription: {
+    type: String,
+    required: [true, "Enter Product Description"],
+  },
+  productRating: {
+    type: Number,
+    default: 4,
+  },
+  productImage: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+  },
+  modifiedAt: {
+    type: Date,
+  },
+});
+
+module.exports = Product = mongoose.model("Product", productSchema);
