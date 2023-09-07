a3943462370734007f8183c6ff7b8b38d395191c
atuljha381 <mail.atuljha@gmail.com>
2023-09-07 22:40:52 +0530

    Context implemented partial - switching to auth/atul

--- TSC-Frontend/src/RegisterViaMail/LoginViaMail.jsx
+++ TSC-Frontend/src/RegisterViaMail/LoginViaMail.jsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import classes from "./registerViaMail.module.css";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../contexts/userContext";
 
 export default function LoginViaMail() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userData, setUserData] = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let token = localStorage.getItem("auth-token");
+    if (token) {
+      navigate("/home");
+    }
+  }, [navigate]);
 
   const loginToUser = (event) => {
     event.preventDefault();
-    
   };
   return (
     <>
